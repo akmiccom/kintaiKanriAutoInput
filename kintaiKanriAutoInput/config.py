deab193b4d07b252f82b3daa52cf70d5a77fab7c
akmiccom <akmiccom@gmail.com>
2023-06-02 16:56:51 +0900

    first commit

---
+++ kintaiKanriAutoInput/config.py
@@ -0,0 +1,12 @@
+
+CLOUD_GATE_URL = r'https://echizen.cloudgate.jp/user-hub/yachiyo-ind/'
+
+GMAIL_URL = r'https://mail.google.com/a/yachiyo-ind.co.jp/'
+
+CALENDAR_URL = r'https://calendar.google.com/calendar/u/0/r?tab=mc&pli=1'
+
+KOBETSU_GENKA_URL = r'https://echizen.cloudgate.jp/sso/yachiyo-ind/post.xhtml?providerUuid=284c5466-7256-49e3-99ab-04ff1a8178a8'
+
+KINTAIKANRI_URL = r'https://yachiyo-ind.obic7.obicnet.ne.jp/Obic7FederationService'
+
+GUEST_WIFI_URL = r'https://sites.google.com/yachiyo-ind.co.jp/yachiyo-portal/guest-wifi'
